15a0e81bee2b2be600f3863c5083c8e8f230228c
linhlk <khanhlinhle2809@gmail.com>
2020-05-20 23:48:43 +0700

    second update

--- main.js
+++ main.js
@@ -1,29 +1,32 @@
 let newsList = [];
 const apiKey = "6d2c1467bedb46a5a41bfd03f84933b0";
+
 const loadNews = async() => {
-    let urlApiKey = `https://newsapi.org/v2/everything?q=movie&sortBy=publishedAt&apiKey=${apiKey}`
+    let urlApiKey = `https://newsapi.org/v2/everything?q=movie&sortBy=publishedAt&apiKey=${apiKey}&page=1`
     let data = await fetch(urlApiKey)
     let result = await data.json();
     newsList = result.articles;
-    console.log(newsList);
     render(newsList);
 }
 
 const render = (list) => {
-    let newsHtml = list.map(item => `<div id="news">
-    <div id="contentsArea">
-        <h2 id="title">${item.title}</h2>
-        <p class="text-muted" id="source">${item.description}</p>
-        <div id="publishedAt">${publicTime(item.publishedAt)}</div>
-        <a href="${item.url}">Read more</a>
-    </div>
-    <div id="imgArea">
-        <img src="${item.urlToImage}" width="400px" />
-    </div>
-</div>`).join("")
+    let newsHtml = list.reduce((preVal, item) => {
+        return preVal +=
+            `<div id="news">
+                <div id="contentsArea">
+                    <h2>${item.title}</h2>
+                    <h6>${item.source.name}</h6>
+                    <p class="text-muted">${item.description}</p>
+                    <div>${publicTime(item.publishedAt)}</div>
+                    <a href="${item.url}">Read more</a>
+                </div>
+                <a target="_blank" href="${item.urlToImage}">
+                    <img src="${item.urlToImage}" width="300px" />
+                </a>
+            </div>\n`;
+    }, "");
 
-    document.getElementById("newsArea").innerHTML = newsHtml;
-
+    document.getElementById("newsArea").innerHTML += newsHtml;
 }
 
 function publicTime(time) {
@@ -34,3 +37,26 @@ function publicTime(time) {
 }
 
 loadNews();
+
+let pageNo = 2;
+let totalNews = 20;
+
+const loadMoreNews = async() => {
+    let urlApiKey = `https://newsapi.org/v2/everything?q=movie&sortBy=publishedAt&apiKey=${apiKey}&page=${pageNo}`
+    let data = await fetch(urlApiKey)
+    let result = await data.json();
+    let list = result.articles;
+
+    if (list.length > 0) {
+        if (pageNo < 5) {
+            render(list);
+            pageNo++;
+            totalNews += list.length;
+
+            for (const news of list) {
+                newsList.push(news);
+            }
+            document.getElementById("totalNews").innerHTML = `Total news: ${totalNews}`;
+        }
+    }
+}
